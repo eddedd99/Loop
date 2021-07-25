1365bfe719347ef59fb3bf3f7f59cddd7357360f
Edd <edcruces99@gmail.com>
2021-07-25 09:32:28 -0500

    Add files via upload

---
+++ Loop/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Loop
+{
+    partial class frmLoop
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEjecutar = new System.Windows.Forms.Button();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnEjecutar
+            // 
+            this.btnEjecutar.Location = new System.Drawing.Point(36, 33);
+            this.btnEjecutar.Name = "btnEjecutar";
+            this.btnEjecutar.Size = new System.Drawing.Size(107, 53);
+            this.btnEjecutar.TabIndex = 0;
+            this.btnEjecutar.Text = "&Ejecutar";
+            this.btnEjecutar.UseVisualStyleBackColor = true;
+            this.btnEjecutar.Click += new System.EventHandler(this.btnEjecutar_Click);
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(149, 33);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(217, 53);
+            this.txtOutput.TabIndex = 1;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(36, 105);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(220, 121);
+            this.listBox1.TabIndex = 2;
+            // 
+            // frmLoop
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(373, 235);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.btnEjecutar);
+            this.Name = "frmLoop";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Loop";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEjecutar;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Loop/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Diagnostics;
+
+namespace Loop
+{
+    public partial class frmLoop : Form
+    {
+        public frmLoop()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEjecutar_Click(object sender, EventArgs e)
+        {
+            byte a = 0;
+            Cursor.Current = Cursors.WaitCursor;
+            for (long i = 1; i <= 10000; i++)
+            {
+                //txtOutput.Text = txtOutput.Text + " " + i.ToString();
+                listBox1.Items.Add(i);
+            }
+            Cursor.Current = Cursors.Default;
+
+            //txtOutput.Text = txtOutput.Text + DateTime.Now.ToString("dddd, dd MMMM yyyy HH:mm:ss") + Environment.NewLine + " ";
+
+            /*
+             * Medir el tiempo de ejecución (2021-07-13)
+             * 
+             * */
+            Stopwatch stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+            Thread.Sleep(1000);
+            stopwatch.Stop();
+
+            txtOutput.Text = "Tiempo en milisecs: " + stopwatch.ElapsedMilliseconds.ToString();
+            
+        }
+    }
+}
